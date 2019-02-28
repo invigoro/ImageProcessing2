3ffeb77b79ff50c9fc5cb9890dd6c7d2c9fba858
Timothy Wells <35319035+invigoro@users.noreply.github.com>
2019-02-28 16:32:39 -0700

    Welcome to the library

--- candycounter.py
+++ candycounter.py
@@ -7,6 +7,7 @@ import numpy as np
 #from matplotlib import cm
 #from sklearn.cluster import MiniBatchKMeans
 
+kernel = np.ones((5,5), np.uint8)
 image = None
 
 try:
@@ -34,7 +35,15 @@ else:
     
 finally:
     #print("yeehaw")
-    image = cv.GaussianBlur(image,(11,11),0)
+    #image = cv.GaussianBlur(image,(11,11),0)
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+    image = cv.blur(image, (3,3))
+    print("yeet")
+
+    
+ 
+
     z = image.reshape((-1,3))
     z = np.float32(z)
 
@@ -46,6 +55,39 @@ finally:
     res = center[label.flatten()]
     res2 = res.reshape((image.shape))
 
+    
+    canny = cv.Canny(res2, 150, 300)
+    
+    '''circles = cv.HoughCircles(canny, cv.HOUGH_GRADIENT, 1.2, 10)
+    # ensure at least some circles were found
+    if circles is not None:
+	# convert the (x, y) coordinates and radius of the circles to integers
+	    circles = np.round(circles[0, :]).astype("int")
+    
+	# loop over the (x, y) coordinates and radius of the circles
+    for (x, y, r) in circles:
+        cv.circle(gray, (x, y), r, (0, 255, 0), 4)
+        cv.rectangle(gray, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)'''
+
+    img1, contours, hierarchy = cv.findContours(canny, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    
+    cv.drawContours(res2, contours, -1, (0,255,0), 3)
+
+    for i in contours:
+        length = cv.arcLength(i, False)
+        if length > 100:
+            '''x,y,w,h = cv.boundingRect(i)
+            cv.rectangle(res2, (x,y),(x+w,y+h),(0,0,255),4)'''
+            (x,y),radius = cv.minEnclosingCircle(i)
+            center = (int(x),int(y))
+            radius = int(0.6*radius)
+            if radius > 5 and radius < 50:
+                cv.circle(res2,center,radius,(0,0,255),2)
+
+
     cv.imshow('Blurred & Color Reduced', res2)
+    cv.imshow('Canny', canny)
+    cv.imshow('Gray', gray)
     cv.waitKey(0)
     cv.destroyAllWindows()
